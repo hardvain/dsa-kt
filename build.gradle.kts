8dc054082a397f48dab4b2b30720e7382beae98a
Aravindh Sridaran <mail@aravindh.io>
2019-07-16 18:01:57 +0200

    Added arrow dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,10 +9,15 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    jcenter()
+    maven("https://dl.bintray.com/arrow-kt/arrow-kt/" )
+    maven("https://oss.jfrog.org/artifactory/oss-snapshot-local/" )
 }
+val arrow_version = "0.9.1-SNAPSHOT"
 
 dependencies {
     compile(kotlin("stdlib-jdk8"))
+    compile("io.arrow-kt:arrow-core-data:$arrow_version")
 }
 
 tasks.withType<KotlinCompile> {
